7fe9468d7d10d7b2697d7492891bc0ef7ef5a800
dd-bts <Giraud16726@lasalle63.fr>
2025-11-26 11:52:34 +0100

    ModerationController, route /moderation/dashboard, envoie des données users et publications, à retravailler quand on aura le json pour supprimer les données inutiles

--- src/Controller/ModerationController.php
+++ src/Controller/ModerationController.php
@@ -6,6 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+use App\Service\ApiLinker;
+
 final class ModerationController extends AbstractController
 {
 
