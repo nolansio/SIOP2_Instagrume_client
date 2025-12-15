7f297e03cac593722aa03b169e34d662e263467e
TolMen Jy <cotton.frachisse@gmail.com>
2025-12-15 17:12:15 +0100

    feat: page détaillée publication avec système commentaires et réponses

---
+++ src/Controller/PublicationViewController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use App\Service\PublicationService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class PublicationViewController extends AbstractController
+{
+    private PublicationService $publicationService;
+
+    public function __construct(PublicationService $publicationService)
+    {
+        $this->publicationService = $publicationService;
+    }
+
+    #[Route('/publication/{id}', name: 'app_publication_view')]
+    public function view(int $id, Request $request): Response
+    {
+        $token = $request->getSession()->get('token');
+        $user = $request->getSession()->get('user');
+
+        try {
+            $publication = $this->publicationService->getPublicationById($id, $token);
+        } catch (\Exception $e) {
+            $this->addFlash('danger', 'Publication non trouvée.');
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('publication/view.html.twig', [
+            'publication' => $publication,
+            'currentUser' => $user
+        ]);
+    }
+}
